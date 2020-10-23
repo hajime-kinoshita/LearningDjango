ec662585b74ecd2503752fa7d7f3b48a83822a41
hajime_kinoshita <kinoshita@e-isys.co.jp>
2020-10-23 15:40:37 +0900

    テンプレート使用

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-#    return HttpResponse("Hello, Django!")
-
-
-#    now = datetime.now()
-
-#    html_content = "<html><head><title>Hello, Django</title></head><body>"
-#    html_content += "<strong>Hello Django</strong> on " + now.strftime("%A, %d %B, %Y at %X")
-#    html_content += "</body></html>"
-
-#    return HttpResponse(html_content)
-
-
     now = datetime.now()
 
     return render(
@@ -29,3 +16,25 @@ def index(request):
             'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
         }
     )
+
+def about(request):
+    return render(
+        request, 
+        "HelloDjangoApp/about.html", 
+        {
+            'title' : "About HelloDjangoApp", 
+            'content' : "Example app page for Django"
+        }
+    )
+
+#    now2 = datetime.now()
+
+#    return render(
+#        request,
+#        "HelloDjangoApp/index.html",
+#        {
+#            'title' : "Hello Django",
+#            'message' : "Hello Django!",
+#            'content' : " on " + now2.strftime("%A, %d %B, %Y at %X")
+#        }
+#    )
